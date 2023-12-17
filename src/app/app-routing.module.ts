f08f56701c72e6d1806610088fcf045cb292ab27
Mitvirani310 <meetvirani310@gmail.com>
2023-12-17 14:42:54 +0530

    Home page 3 Button

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,10 +6,11 @@ import { CityComponent } from './city/city.component';
 import { ProductDetailComponent } from './product/product-detail/product-detail.component';
 import { CityDetailComponent } from './city/city-detail/city-detail.component';
 import { UserDetailComponent } from './user/user-detail/user-detail.component';
+import { HomeContentComponent } from './home-content/home-content.component';
 
 const routes: Routes = [
+  { path: '', component: HomeContentComponent },
   { path: 'products', component: ProductComponent },
-  { path: '', component: ProductComponent },
   { path: 'users',component: UserComponent},
   { path: 'cities',component: CityComponent},
   { path: 'products/:id',component : ProductDetailComponent},
